a162fa69e9dee98e2221340258486693ecd76614
michael adeyemi <michael.amch777@gmail.com>
2016-11-15 17:11:23 -0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+   
+    var mySwiper = new Swiper ('.swiper-container', {
+        autoplay: 5000,
+        loop: true,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev'
+    });
+    
+});
