d1f092dade6c732df9eec5b22a7658eaa41b337b
michael adeyemi <michael.amch777@gmail.com>
2016-11-17 01:40:36 -0600

    add responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
-$(function(){
+$('a[href*="#main"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
+
+$( function() {
+    $( ".form-control.date" ).datepicker();
+  } );
+  
+  $(function(){
    
     var mySwiper = new Swiper ('.swiper-container', {
         autoplay: 5000,
